860245dcca9cb87edc92bb75d69ebd0e1330240d
unknown <brenos93@gmail.com>
2022-11-04 16:13:24 -0300

    feat: fiz a criação das rotas de professionals, teacher e address

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
+import addressRoutes from "./routes/address.routes";
+import professionalRoutes from "./routes/professionals.routes";
+import teacherRoutes from "./routes/teacher.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/professionals", professionalRoutes);
+app.use("/address", addressRoutes);
+app.use("/teacher", teacherRoutes);
 
 app.use(handleErrorMiddleware);
 
