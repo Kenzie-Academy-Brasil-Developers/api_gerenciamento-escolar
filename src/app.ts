c120cbd847a78f4704d7bd1a1e6b63c2d0c8f277
VictrCruz312 <motoggb979@gmail.com>
2022-11-04 11:01:56 -0300

    fix: removendo rota que não existe do app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from "express";
 import "reflect-metadata";
-import routesSchoolMaterials from "./routes/schoolMaterials.routes"
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
-
 
 const app = express();
 
 app.use(express.json());
-app.use(routesSchoolMaterials)
 
 app.use(handleErrorMiddleware);
 
