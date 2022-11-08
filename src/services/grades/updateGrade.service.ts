8f5dd3fe52b69ef2491e7fd3d11d6e02c6017587
gabriel <gabrielmineironogueira@gmail.com>
2022-11-07 21:18:22 -0300

    feat/EmAndamento

--- src/services/grades/updateGrade.service.ts
+++ src/services/grades/updateGrade.service.ts
@@ -5,10 +5,7 @@ import { Grades } from "../../entities/grades.entity";
 import { SchoolGrades } from "../../entities/schoolGrades.entity";
 import { appError } from "../../errors/appError";
 
-const gradeUpdateService = async (
-  data: IHistoryUpdate,
-  id: string
-) => {
+const gradeUpdateService = async (data: IHistoryUpdate, id: string) => {
   const studentRepository = AppDataSource.getRepository(Students);
 
   const returnStd = await studentRepository.findOneBy({
@@ -18,16 +15,27 @@ const gradeUpdateService = async (
   if (!returnStd) {
     throw new appError("Student not found", 404);
   }
-  
+
   await studentRepository.save({
     id,
-    school_subject: data.school_subject ? data.school_subject : returnStd.gradeHistory.grade.school_subject,
-    firstGrade: data.firstGrade ? data.firstGrade : returnStd.gradeHistory.grade.firstGrade,
-    secondGrade: data.secondGrade ? data.secondGrade : returnStd.gradeHistory.grade.secondGrade,
-    thirdGrade: data.thirdGrade ? data.thirdGrade : returnStd.gradeHistory.grade.thirdGrade,
-    fourthGrade: data.fourthGrade ? data.fourthGrade : returnStd.gradeHistory.grade.fourthGrade,
-    absences: data.absences ? data.absences : returnStd.gradeHistory.grade.absences,
-
+    school_subject: data.school_subject
+      ? data.school_subject
+      : returnStd.gradeHistory.grade.school_subject,
+    firstGrade: data.firstGrade
+      ? data.firstGrade
+      : returnStd.gradeHistory.grade.firstGrade,
+    secondGrade: data.secondGrade
+      ? data.secondGrade
+      : returnStd.gradeHistory.grade.secondGrade,
+    thirdGrade: data.thirdGrade
+      ? data.thirdGrade
+      : returnStd.gradeHistory.grade.thirdGrade,
+    fourthGrade: data.fourthGrade
+      ? data.fourthGrade
+      : returnStd.gradeHistory.grade.fourthGrade,
+    absences: data.absences
+      ? data.absences
+      : returnStd.gradeHistory.grade.absences,
   });
 
   const resultStd = await studentRepository.findOneBy({
