398c314184731973f45e8be936a9642468d7ff59
VictrCruz312 <motoggb979@gmail.com>
2022-11-09 11:09:27 -0300

    fix

--- src/services/students/updateStudent.services.ts
+++ src/services/students/updateStudent.services.ts
@@ -1,7 +1,7 @@
 import AppDataSource from "../../data-source";
 import { Students } from "../../entities/student.entity";
 import { appError } from "../../errors/appError";
-import { hash } from "bcrypt";
+import { hash } from "bcryptjs";
 const updateStudentService = async (id: string, userData: any) => {
   const studentRepository = AppDataSource.getRepository(Students);
 
